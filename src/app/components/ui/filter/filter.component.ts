6fd8a1efbe669d46aa82fe3bb4ecaad108dfc333
Button-20 <jasonaddy51@gmail.com>
2022-03-12 12:47:00 +0000

    Review Comments Done

--- src/app/components/ui/filter/filter.component.ts
+++ src/app/components/ui/filter/filter.component.ts
@@ -40,36 +40,11 @@ export class FilterComponent implements OnInit {
     this._listingservices.listings = this._listingservices.oldlistings
   }
 
-<<<<<<< HEAD
-  async pushToArray(objects: any, array: any[]){
-    for (var prop in objects) {
-      if (objects.hasOwnProperty(prop)) {
-        var innerObj: any = {};
-        innerObj[prop] = objects[prop];
-        array.push(innerObj[prop]);
-      }
-    }
-  }
-
-  async useFilter(data: any){
-    const query = (<HTMLInputElement>document.getElementById(data?.gender)).checked;
-    if (query) {
-      this.filter.push(data);
-    } else {
-      var index = this.filter.indexOf(data); 
-      if (index !== -1){
-        this.filter.splice(index, 1);
-      }
-    }
-    this._listingservices.filterListings(this._listingservices.listings, this.filter)
-    // console.log(this.filter)
-=======
   async useFilter(){
     const listing: any = await this._listingservices.filterProducts(
       this._listingservices.filterSearch, this._listingservices.listings
     )
     this._listingservices.listings = listing
     if(listing.length == 0) this.clearFilter()
->>>>>>> aa7490be2c24d1a40810607b19c88e0cfb793821
   }
 }
