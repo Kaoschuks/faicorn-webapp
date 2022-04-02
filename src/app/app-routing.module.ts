ebdcc07a2002b1535dbd5f3d4d3e197600b23b35
Button-20 <jasonaddy51@gmail.com>
2022-04-02 14:09:12 +0000

    Guard Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { IndexLayoutComponent } from './components/layout/index-layout/index-layout.component';
+import { ProfileGuardsService } from './guards/profile.guard';
 
 const routes: Routes = [
   { 
     path: '', 
     component: IndexLayoutComponent,
-    loadChildren: () => import('./pages/website/website.module').then( m => m.WebsiteModule) 
+    loadChildren: () => import('./pages/website/website.module').then( m => m.WebsiteModule),
+    canActivate: [ProfileGuardsService]
   },
   { path: '**', redirectTo: '404' }
 ];
---
+++ src/app/guards/profile.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { CanActivate,  Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { GlobalsService } from '../services/core/globals.service';
+import { UsersService } from '../services/features/users';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileGuardsService implements CanActivate {
+  constructor(
+    public globals: GlobalsService, 
+    public uData: UsersService,
+    public router: Router,
+    public toastr: ToastrService
+  ) {
+  }
+
+  async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+    setTimeout(async () => {
+      var user = await this.globals.storage.getItem('user');
+      var userLoggedIn = await this.uData.isLoggedOn();
+      if (user && userLoggedIn && user?.fullname === '') {
+        this.toastr.warning('Please complete your profile', 'User Profile Required!')
+        this.router.navigate(['/accounts/profile']);
+      }
+      return false;
+    }, 100);
+
+  return true;
+
+  }
+
+}
