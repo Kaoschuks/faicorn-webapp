ec58dec58cbe8f17197c1ab78ea16cbaab4fd695
Button-20 <jasonaddy51@gmail.com>
2022-04-18 18:25:19 +0000

    a

--- src/app/components/accounts/security/security.component.ts
+++ src/app/components/accounts/security/security.component.ts
@@ -5,23 +5,24 @@ import { UsersService } from 'src/app/services/features/users';
 @Component({
   selector: 'accounts-security',
   templateUrl: './security.component.html',
-  styleUrls: ['./security.component.css']
+  styleUrls: ['./security.component.css'],
 })
 export class SecurityComponent implements OnInit {
-
   constructor(
     private usersService: UsersService,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  async deleteUser(){
-    if(confirm('Are you sure you want to delete your account permanently?') == true){
+  async deleteUser() {
+    if (
+      confirm('Are you sure you want to delete your account permanently?') ==
+      true
+    ) {
       await this.usersService.deleteUser();
       await this.usersService.logout();
-      this.toastr.success('User deleted successfully.', 'Delete Ad!');
-	  }
+      this.toastr.success('User Account deleted successfully.', 'Delete Ad!');
+    }
   }
 }
