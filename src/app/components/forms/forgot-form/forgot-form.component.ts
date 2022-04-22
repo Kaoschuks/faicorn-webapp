1fd377474cc34f9cc9d1d6061afddda3eb390fff
Button-20 <jasonaddy51@gmail.com>
2022-04-22 13:59:44 +0000

    a

--- src/app/components/forms/forgot-form/forgot-form.component.ts
+++ src/app/components/forms/forgot-form/forgot-form.component.ts
@@ -5,38 +5,38 @@ import { UsersService } from 'src/app/services/features/users';
 @Component({
   selector: 'forgot-form',
   templateUrl: './forgot-form.component.html',
-  styleUrls: ['./forgot-form.component.css']
+  styleUrls: ['./forgot-form.component.css'],
 })
 export class ForgotFormComponent implements OnInit {
-
   forgotForm: FormGroup = new FormGroup({
     email: new FormControl(
-      "",
+      '',
       Validators.compose([
         Validators.required,
-        Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
       ])
-    )
+    ),
   });
   validation_messages = {
     email: [
-      { type: "required", message: "Email is required." },
+      { type: 'required', message: 'Email is required.' },
       {
-        type: "minlength",
-        message: "Email must be at least 5 characters long."
+        type: 'minlength',
+        message: 'Email must be at least 5 characters long.',
       },
-      { type: "pattern", message: "Email must be valid." },
-      { type: "email", message: "Email must be valid" }
-    ]
+      { type: 'pattern', message: 'Email must be valid.' },
+      { type: 'email', message: 'Email must be valid' },
+    ],
   };
 
-  error: string = ''
-  constructor(
-    public _userService: UsersService
-  ) {
-  }
+  error: string = '';
+  constructor(public _userService: UsersService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  OnSubmit() {}
+
+  get email() {
+    return this.forgotForm.get('email');
+  }
 }
