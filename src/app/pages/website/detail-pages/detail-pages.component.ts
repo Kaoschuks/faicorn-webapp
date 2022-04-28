95498db46355f18016307acb4fb6eca7bd5ae3ec
Button-20 <jasonaddy51@gmail.com>
2022-04-28 22:19:28 +0000

    a

--- src/app/pages/website/detail-pages/detail-pages.component.ts
+++ src/app/pages/website/detail-pages/detail-pages.component.ts
@@ -1,25 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { Event, NavigationStart} from '@angular/router';
+import { Event, NavigationStart } from '@angular/router';
 import { GlobalsService } from 'src/app/services/core/globals.service';
 import { ListingsService } from 'src/app/services/features/listings/listings.service';
+import { MessagingService } from 'src/app/services/features/messaging/messaging.service';
+import { UsersService } from 'src/app/services/features/users';
 
 @Component({
   selector: 'app-detail-pages',
   templateUrl: './detail-pages.component.html',
-  styleUrls: ['./detail-pages.component.css']
+  styleUrls: ['./detail-pages.component.css'],
 })
 export class DetailPagesComponent implements OnInit {
-
   showContact: boolean = false;
-  url: any = this._global.url.split('/')
+  url: any = this._global.url.split('/');
   constructor(
     private _global: GlobalsService,
     public _listingservices: ListingsService,
-  ) { }
+    public messagingServices: MessagingService
+  ) {}
 
   async ngOnInit() {
-    if(this.url.length === 4) await this._listingservices.getlistings('/all', `?brands=${this.url[this.url.length - 2]}&limit=1000`)
-    if(this.url.length === 4) await this._listingservices.getlistings(`/${this.url[this.url.length - 1]}`, ``, 'single')
+    if (this.url.length === 4)
+      await this._listingservices.getlistings(
+        '/all',
+        `?brands=${this.url[this.url.length - 2]}&limit=1000`
+      );
+    if (this.url.length === 4)
+      await this._listingservices.getlistings(
+        `/${this.url[this.url.length - 1]}`,
+        ``,
+        'single'
+      );
     // this.getAds()
   }
 
@@ -27,15 +38,40 @@ export class DetailPagesComponent implements OnInit {
     this._global.router.events.subscribe(async (event: Event) => {
       if (event instanceof NavigationStart) {
         let url = event.url.split('/');
-        if(url.length === 4) await this._listingservices.getlistings('/all', `?brands=${url[url.length - 2]}&limit=1000`)
-        if(url.length === 4) await this._listingservices.getlistings(`/${url[url.length - 1]}`, ``, 'single')
+        if (url.length === 4)
+          await this._listingservices.getlistings(
+            '/all',
+            `?brands=${url[url.length - 2]}&limit=1000`
+          );
+        if (url.length === 4)
+          await this._listingservices.getlistings(
+            `/${url[url.length - 1]}`,
+            ``,
+            'single'
+          );
       }
     });
   }
 
   toogle() {
-    console.log(this._listingservices?.listingInfo?.user)
-    this.showContact = (!this.showContact) ? true : false ;
+    console.log(this._listingservices?.listingInfo?.user);
+    this.showContact = !this.showContact ? true : false;
   }
 
+  async sendMessage() {
+    let channel = {
+      channel_name: this._listingservices?.listingInfo?.name,
+      users: [
+        {
+          uid: this._listingservices?.listingInfo?.user?.uid,
+        },
+      ],
+      image: this._listingservices?.listingInfo?.images[0],
+    };
+
+    const resp: any = await this.messagingServices.postChannels(channel);
+    await this._global.router.navigate([
+      'accounts/messages/' + resp.channel_id,
+    ]);
+  }
 }
--- src/app/services/features/messaging/messaging.service.ts
+++ src/app/services/features/messaging/messaging.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { GlobalsService } from '../../core/globals.service';
 import { RequestService } from '../../core/request-service';
 
@@ -9,12 +10,16 @@ export class MessagingService {
   messageInfo: any;
   messages: Array<any> = [];
 
-  constructor(private globals: GlobalsService, private api: RequestService) {}
+  constructor(
+    private globals: GlobalsService,
+    private api: RequestService,
+    private toastr: ToastrService
+  ) {}
 
-  postMessages(data: any) {
+  async postMessages(data: any) {
     return new Promise(async (resolve: any, reject: any) => {
       try {
-        const resp: any = await this.api.post('/message', data);
+        const resp: any = await this.api.post('message', data);
         if (resp.error) throw new Error(resp.error);
         resolve(resp.message);
       } catch (ex: any) {
@@ -30,7 +35,7 @@ export class MessagingService {
   async deleteMessage(id: string = '') {
     return await new Promise(async (resolve, reject) => {
       try {
-        const resp: any = await this.api.delete(`/message/${id}`);
+        const resp: any = await this.api.delete(`message/${id}`);
         if (resp.error) throw new Error(resp.error || resp);
 
         resolve(resp.message);
@@ -44,10 +49,10 @@ export class MessagingService {
     });
   }
 
-  getChannels() {
+  async getChannels() {
     return new Promise(async (resolve: any, reject: any) => {
       try {
-        const resp: any = await this.api.get('/channels');
+        const resp: any = await this.api.get('channels');
         if (resp.error) throw new Error(resp.error);
         resolve(resp.message);
       } catch (ex: any) {
@@ -60,12 +65,11 @@ export class MessagingService {
     });
   }
 
-  postChannels(data: any) {
+  async postChannels(data: any) {
     return new Promise(async (resolve: any, reject: any) => {
       try {
-        const resp: any = await this.api.post('/channels', data);
-        if (resp.error) throw new Error(resp.error);
-        resolve(resp.message);
+        const resp: any = await this.api.post('channels', data);
+        resolve(resp);
       } catch (ex: any) {
         this.globals.spinner.hide();
         console.log(ex);
@@ -76,10 +80,10 @@ export class MessagingService {
     });
   }
 
-  getChannel(id: string = '') {
+  async getChannel(id: string = '') {
     return new Promise(async (resolve: any, reject: any) => {
       try {
-        const resp: any = await this.api.get(`/channels/${id}`);
+        const resp: any = await this.api.get(`channels/${id}`);
         if (resp.error) throw new Error(resp.error);
         resolve(resp.message);
       } catch (ex: any) {
@@ -87,6 +91,24 @@ export class MessagingService {
         console.log(ex);
         reject({
           error: ex.error || ex.message || ex || 'Error getting channel: ' + id,
+        });
+      }
+    });
+  }
+
+  async deleteChannel(id: string = '') {
+    return await new Promise(async (resolve, reject) => {
+      try {
+        const resp: any = await this.api.delete(`channels/${id}`);
+        if (resp.error) throw new Error(resp.error || resp);
+
+        resolve(resp.message);
+      } catch (ex: any) {
+        this.globals.spinner.hide();
+        this.toastr.error(ex, 'Error Deleting Channel!');
+        console.log(ex);
+        reject({
+          error: ex.error || ex.message || ex || 'Error deleting channel',
         });
       }
     });
